c61c10c345c2c1bbdfe4aadb3ca469855f577765
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-12-25 16:28:20 -0500

    Set up backbone for index.php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-Sandwiches
+<!DCOTYPE html>
+<html>
+<head>
+<title>MyTube</title>
+
+<link rel="stylesheet" type="text/css" href="assets/css/style.css"> 
+</head>
+<body>
+
+    <div id="pageContainer">
+        
+        <div id="mastHeadContainer">
+            Hello!
+        </div>
+        
+        <div id="sideNavContainer">
+        
+        </div>
+
+        <div 9d="mainSectionContainer">
+        
+        </div>
+
+    </div>
+</body>
+</html>
