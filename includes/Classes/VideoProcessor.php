d44d0c7b3127b1a7388758fc39b4daabac863051
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-12-28 13:37:06 -0500

    Call the upload function

---
+++ includes/Classes/VideoProcessor.php
@@ -0,0 +1,9 @@
+<?php
+
+class VideoPrcossor {
+
+    private $con;
+    public function __construct(){
+        $this->con = $con;
+    }
+}
--- processing.php
+++ processing.php
@@ -1,5 +1,7 @@
 <?php 
 require_once("includes/header.php");
+require_once("includes/classes/VideoUploadData.php");
+require_once("includes/classes/VideoProcessor.php");
 
 if(!isset($_POST["uploadButton"])){
     echo"No file sent to ppage.";
@@ -8,8 +10,11 @@ if(!isset($_POST["uploadButton"])){
 
 //1. Create file upload data
 
-$videoUploadDate = new VideoUploadData($_POST["fileInput"], $_POST["title"], $_POSt["descriptionInput"], $POST["privacyInput"], $POST["categoryInput"], "REPLACE_THIS");
+$videoUploadData = new VideoUploadData($_POST["fileInput"], $_POST["title"], $_POSt["descriptionInput"], $POST["privacyInput"], $POST["categoryInput"], "REPLACE_THIS");
 
 //2. Process video data (upload)
+
+$videoProcessor = new VideoProcessor($con);
+$wasSuccesssful = $videoProcessor->upload();
 //3. Check if upload was successful 
 ?>
