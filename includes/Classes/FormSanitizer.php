35ce7a40a1ba881f70a7a2c8f3854ba3d0b58709
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-02-04 21:42:19 -0500

    Finish up sanitizer functions for all input fields

--- includes/Classes/FormSanitizer.php
+++ includes/Classes/FormSanitizer.php
@@ -9,5 +9,21 @@ class FormSanitizer {
         return $inputText;
     }
 
+    public static function sanitizeFormUsername($inputText) {
+        $inputText = strip_tags($inputText);
+        $inputText = str_replace(" ", "", $inputText);
+        return $inputText;
+    }
+
+    public static function sanitizeFormPassword($inputText) {
+        $inputText = strip_tags($inputText);
+        return $inputText;
+    }
+
+    public static function sanitizeFormEmail($inputText) {
+        $inputText = strip_tags($inputText);
+        $inputText = str_replace(" ", "", $inputText);
+        return $inputText;
+    }
 }
 ?>
